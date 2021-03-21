a0c2c6e407dc0c815028329c2e5a030a5cb392d1
kywsc <fireyw@gmail.com>
2021-03-21 21:55:13 +0900

    -paypal api create

---
+++ server/models/Payment.js
@@ -0,0 +1,25 @@
+//-누가 어떤상품에 결제 했는지
+//-paypal에서 결제 후 제공되는 정보 저장
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const paymentSchema = mongoose.Schema({
+    user:{
+        type:Array,
+        default:[]
+    },
+    data:{
+        type:Array,
+        default: []
+    },
+    product:{
+        type:Array,
+        default:[]
+    }
+}, {timestamps:true})
+
+
+const Payment = mongoose.model('Payment', productSchema);
+
+module.exports = { Payment }
