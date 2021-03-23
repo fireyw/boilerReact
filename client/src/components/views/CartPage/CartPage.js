53876e7e38453ec34ff78d44995885332d4dd020
kywsc <fireyw@gmail.com>
2021-03-23 20:56:17 +0900

    -callback payment

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,13 +2,14 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch} from "react-redux";
 import {getCartItems, removeCartItem, onSuccessBuy} from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
-import {Empty} from 'antd';
+import {Empty, Result} from 'antd';
 import Paypal from "../../utils/Paypal";
 
 function CartPage(props) {
     const dispatch = useDispatch();
     const [Total, setTotal] = useState(0);
     const [ShowTotal, setShowTotal] = useState(false);
+    const [ShowSuccess, setShowSuccess] = useState(false);
 
     useEffect(() => {
 
@@ -56,7 +57,8 @@ function CartPage(props) {
         }))
             .then(response => {
                 if (response.payload.success) {
-
+                    setShowTotal(false)
+                    setShowSuccess(true)
                 }
             })
     }
@@ -73,12 +75,19 @@ function CartPage(props) {
                 <div style={{marginTop: '3rem'}}>
                     <h2>Total Amount: ${Total}</h2>
                 </div>
-                :
-                <>
-                    <br/>
-                    <Empty description={false}/>
-                </>
+                : ShowSuccess ?
+                    <Result
+                        status="success"
+                        title="Successfully Purchased Cloud Server ECS!"
+                    />
+                    :
+                    <>
+                        <br/>
+                        <Empty description={false}/>
+                    </>
             }
+
+
             {ShowTotal &&
             <Paypal
                 total={Total}
