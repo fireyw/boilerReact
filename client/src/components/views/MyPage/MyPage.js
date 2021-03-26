ff7f2fb78d72e99c97672762087e8b670ebc7cdc
kywsc <fireyw@gmail.com>
2021-03-26 20:20:29 +0900

    -Mypage grid flex

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function MyPage(props) {
     return (
-        <div>MyPage</div>
+        <div className="container" style={{display:"grid"}}>
+            <div className="item">A</div>
+            <div className="item">B</div>
+            <div className="item">C</div>
+            <div className="item">D</div>
+            <div className="item">E</div>
+            <div className="item">F</div>
+            <div className="item">G</div>
+            <div className="item">H</div>
+            <div className="item">I</div>
+        </div>
+
     );
 }
 
