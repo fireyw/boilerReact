96eebc2c8741611fecac9355e513bfc87cae032c
kywsc <fireyw@gmail.com>
2021-03-29 21:26:01 +0900

    -Mypage Typography andt

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 
 function MyPage(props) {
     const wrapperStyles = {
-        // display: 'flex',
-        // justifyContent:'space-between'
         display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+        gridTemplateColumns: '1fr 1fr',
         gridGap: '1rem',
-        minHeight: '500px',
-        backgroundColor: 'ffff00'
+        minHeight: '600px',
+        backgroundColor: '#808080'
     }
     const item={
-        backgroundColor:"#ff0000"
+        backgroundColor:"#ffffffff",
+        margin: '1rem'
     }
     return (
-        // <div style={{display:'flex', justifyContent:'space-between'}}>
-        <div style={wrapperStyles}
-        >
-            <div style={item}>프로필1</div>
-            <div>정보</div>
+        <div style={wrapperStyles}>
+            <div style={item}>
+                <Title level={2} style={{margin:'2rem'}}>프로필</Title>
+            </div>
+            <div style={item}>연락처 및 알림</div>
+            <div style={item}>지역설정</div>
             {/*<div className="item">이력</div>*/}
         </div>
 
