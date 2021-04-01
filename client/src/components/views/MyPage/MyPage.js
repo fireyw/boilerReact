996330d1450e6391e005784ed6aa043bf82811ed
kywsc <fireyw@gmail.com>
2021-04-01 21:43:08 +0900

    -Mypage remove antd

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -3,8 +3,8 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 
-
 function MyPage(props) {
+
     const wrapperStyles = {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
@@ -19,11 +19,12 @@ function MyPage(props) {
     return (
         <div style={wrapperStyles}>
             <div style={item}>
-                <Title level={2} style={{margin:'2rem'}}>프로필</Title>
+                <Title level={2} style={{margin:'2rem'}}>프로필
+                </Title>
+
             </div>
             <div style={item}>연락처 및 알림</div>
             <div style={item}>지역설정</div>
-            {/*<div className="item">이력</div>*/}
         </div>
 
     );
