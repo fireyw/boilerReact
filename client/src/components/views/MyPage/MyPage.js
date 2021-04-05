61387d140f4c0b44aa7d8eb1bb0fcb0f345042b3
kywsc <fireyw@gmail.com>
2021-04-05 15:22:24 +0900

    -Mypage andt title, row, col

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Typography } from 'antd';
+import { Typography, Row, Col } from 'antd';
 
 const { Title } = Typography;
 
 function MyPage(props) {
-
+    const dStyle = { background: '#0092ff', paddingLeft: '8px' };
     const wrapperStyles = {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
@@ -21,7 +21,14 @@ function MyPage(props) {
             <div style={item}>
                 <Title level={2} style={{margin:'2rem'}}>프로필
                 </Title>
-
+                <Row gutter={[16,16]}>
+                    <Col lg={12} xs={24} >
+                        <div style={dStyle}>col-6</div>
+                    </Col>
+                    <Col lg={12} xs={24} >
+                        <div style={dStyle}>col-6</div>
+                    </Col>
+                </Row>
             </div>
             <div style={item}>연락처 및 알림</div>
             <div style={item}>지역설정</div>
