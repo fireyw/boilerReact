f50f8339ff364b26f0c16ac14ceea8396da9b837
kywsc <fireyw@gmail.com>
2021-04-07 18:25:46 +0900

    -develop mypage

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Typography, Row, Col, Button } from 'antd';
+import {Typography,Row, Col, Button} from 'antd';
 
-const { Title } = Typography;
+const {Title, Text} = Typography;
 
 function MyPage(props) {
     console.log('mypage : ', props.user.userData);
@@ -15,37 +15,48 @@ function MyPage(props) {
     const innerDivGrid = {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
-        backgroundColor:"#ffffffff",
+        backgroundColor: "#ffffffff",
         margin: '1rem'
     }
 
-    const item={
-        backgroundColor:"#ffffffff",
+    const item = {
+        backgroundColor: "#ffffffff",
         margin: '1rem'
     }
 
     return (
         <div style={wrapperStyles}>
             <div style={innerDivGrid}>
-                <div style={{gridColumn:'1/3', maxHeight:'1em', alignSelf:'start'}}>
-                    <Title level={4} style={{margin:'2rem'}}>프로필
+                <div style={{gridColumn: '1/3', maxHeight: '1em', alignSelf: 'start'}}>
+                    <Title level={4} style={{margin: '2rem'}}>프로필
                     </Title>
                 </div>
-                <div style={{alignSelf:'center', justifySelf:'center'}}>
+                <div style={{alignSelf: 'center', justifySelf: 'center'}}>
                     {props.user.userData &&
-                        <img style={{maxWidth: '80px', width: '80px', height: '80px'}}
-                             src={`http://localhost:5000/${props.user.userData.profileImage}`}/>
+                    <img style={{maxWidth: '80px', width: '80px', height: '80px'}}
+                         src={`http://localhost:5000/${props.user.userData.profileImage}`}/>
                     }
                 </div>
-                <div style={{alignSelf:'center', justifySelf:'start'}}>
+                <div style={{alignSelf: 'center', justifySelf: 'start'}}>
                     별명: {props.user.userData && props.user.userData.nickName}
 
                 </div>
-                <div style={{alignSelf:'end', padding:'1em'}}>
+                <div style={{alignSelf: 'end', padding: '1em'}}>
                     <Button>수정</Button>
                 </div>
             </div>
-            <div style={item}>연락처 및 알림</div>
+            <div style={item}>
+                <div>
+                    <Title level={4} style={{margin: '2rem'}}>
+                        연락처 및 알림
+                    </Title>
+                </div>
+                <div>
+                    <Text level={4} style={{margin: '2rem'}}>
+                        전화번호 : {props.user.userData && props.user.userData.phone}
+                    </Text>
+                </div>
+            </div>
             <div style={item}>지역설정</div>
         </div>
 
